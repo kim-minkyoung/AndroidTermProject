d6a040eec7690b185f5038d8d43c01825eec38f4
김문경 <>
2024-05-28 01:03:41 +0900

    feat : merge 후 login 화면 메인 화면으로 수정

---
+++ app/src/main/java/com/example/androidtermproject/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.androidtermproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtermproject.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val id = binding.ID.text.toString()
+            val password = binding.password.text.toString()
+
+            // 간단한 ID 및 비밀번호 확인 (예제)
+            if (id == "admin" && password == "admin") {
+                val intent = Intent(this, CalendarActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.errorMessage.text = "Invalid ID or Password"
+            }
+        }
+    }
+}
