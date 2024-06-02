9ea13a0707fd61bf733899e34568209466c97f58
김문경 <>
2024-06-03 03:34:49 +0900

    design : 클릭시 배경화면 색 변화 추가

--- app/src/main/java/com/example/androidtermproject/adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/androidtermproject/adapter/MusicAdapter.kt
@@ -1,13 +1,24 @@
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.androidtermproject.R
 import com.example.androidtermproject.mania_api.MusicItem
+
 class MusicAdapter(private val items: List<MusicItem>) : RecyclerView.Adapter<MusicAdapter.ViewHolder>() {
+
+    private var selectedItemPosition: Int = RecyclerView.NO_POSITION
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_song, parent, false)
@@ -16,18 +27,39 @@ class MusicAdapter(private val items: List<MusicItem>) : RecyclerView.Adapter<Mu
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.bind(item)
+        holder.bind(item, position)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MusicItem) {
-            itemView.findViewById<TextView>(R.id.songTitleTextView).text = item.title
-            itemView.findViewById<TextView>(R.id.songArtistTextView).text = item.artist
-            Glide.with(itemView.context).load(item.albumImage).into(itemView.findViewById<ImageView>(R.id.songImageView))
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MusicItem, position: Int) {
+            val titleTextView = itemView.findViewById<TextView>(R.id.songTitleTextView)
+            val artistTextView = itemView.findViewById<TextView>(R.id.songArtistTextView)
+            val albumImageView = itemView.findViewById<ImageView>(R.id.songImageView)
+
+            titleTextView.text = item.title
+            artistTextView.text = item.artist
+            Glide.with(itemView.context).load(item.albumImage).into(albumImageView)
+
+            itemView.setOnClickListener {
+                if (item.title.isNotEmpty()) {
+                    if (selectedItemPosition != RecyclerView.NO_POSITION) {
+                        val previousSelectedView = recyclerView.findViewHolderForAdapterPosition(selectedItemPosition)?.itemView
+                        previousSelectedView?.setBackgroundColor(Color.TRANSPARENT)
+                    }
+
+                    itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.purplePoint)) // 보라색으로 배경색 변경
+                    selectedItemPosition = adapterPosition
+                }
+            }
+            if (selectedItemPosition == position) {
+                itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.purplePoint)) // 보라색으로 배경색 변경
+            } else {
+                itemView.setBackgroundColor(Color.TRANSPARENT)
+            }
         }
     }
 }
