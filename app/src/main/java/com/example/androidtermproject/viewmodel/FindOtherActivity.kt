0ca1a1073f0bbb5041053212f9ec63c86319c933
kim-minkyoung <decomin02@pusan.ac.kr>
2024-06-04 22:22:25 +0900

    같은 노래 친구 찾음

--- app/src/main/java/com/example/androidtermproject/viewmodel/FindOtherActivity.kt
+++ app/src/main/java/com/example/androidtermproject/viewmodel/FindOtherActivity.kt
@@ -27,16 +27,20 @@ class FindOtherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_others)
 
+        // Firebase 인스턴스 초기화
         auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
 
+        // 액션바 설정
         supportActionBar?.apply {
             title = "Find Others"
             setDisplayHomeAsUpEnabled(true)
         }
 
+        // 인텐트에서 MusicItem 객체 가져오기
         val selectedMusic = intent.getSerializableExtra("selectedMusic") as? MusicItem
 
+        // UI 요소 초기화
         val musicImageView = findViewById<ImageView>(R.id.musicImage)
         val musicTitleTextView = findViewById<TextView>(R.id.musicTitle)
         val musicArtistTextView = findViewById<TextView>(R.id.musicArtist)
@@ -44,10 +48,11 @@ class FindOtherActivity : AppCompatActivity() {
         selectedMusic?.let {
             musicTitleTextView.text = it.title
             musicArtistTextView.text = it.artist
-            Glide.with(this).load(selectedMusic.albumImage).error(R.drawable.default_music_img).into(musicImageView)
+            Glide.with(this).load(it.albumImage).error(R.drawable.default_music_img).into(musicImageView)
         }
 
-        loadUsersMusicBuddies(selectedMusic!!)
+        // 선택된 음악이 유효한 경우에만 사용자 음악 버디를 로드
+        selectedMusic?.let { loadUsersMusicBuddies(it) }
     }
 
     private fun loadUsersMusicBuddies(selectedMusic: MusicItem) {
@@ -55,9 +60,14 @@ class FindOtherActivity : AppCompatActivity() {
 
         usersCollectionRef.get()
             .addOnSuccessListener { querySnapshot ->
+                if (querySnapshot.isEmpty) {
+                    Toast.makeText(this@FindOtherActivity, "같은 음악을 담고 있는 사용자가 없습니다", Toast.LENGTH_SHORT).show()
+                    return@addOnSuccessListener
+                }
+
                 for (userDocument in querySnapshot.documents) {
                     val diariesCollectionRef = userDocument.reference.collection("diaries")
-                    println("diariesCollectionRef: ${diariesCollectionRef}")
+
                     diariesCollectionRef.get()
                         .addOnSuccessListener { diariesQuerySnapshot ->
                             for (diaryDocument in diariesQuerySnapshot.documents) {
@@ -65,30 +75,63 @@ class FindOtherActivity : AppCompatActivity() {
                                 val musicArtist = diaryDocument.getString("musicArtist")
 
                                 if (musicTitle == selectedMusic.title && musicArtist == selectedMusic.artist) {
-                                    val userName = userDocument.id
-                                    musicBuddyList.add(MusicBuddy(userName, "", "https://via.placeholder.com/150"))
+//                                    val userName = userDocument.id
+                                    val profileRef = userDocument.reference.collection("profile")
+
+                                    profileRef.get()
+                                        .addOnSuccessListener { profileSnapshot ->
+                                            // 프로필 서브컬렉션에 문서가 있는지 확인
+                                            if (profileSnapshot.isEmpty) {
+                                                Toast.makeText(this@FindOtherActivity, "불러오지 못한 프로필이 있습니다", Toast.LENGTH_SHORT).show()
+                                                return@addOnSuccessListener
+                                            }
+
+                                            // 프로필 서브컬렉션에 있는 문서를 반복하여 처리
+                                            for (profileDocument in profileSnapshot.documents) {
+                                                // 프로필 문서에서 원하는 필드 가져오기
+                                                val userName = profileDocument.getString("profileName") ?: ""
+                                                val userComment = profileDocument.getString("profileComment") ?: ""
+                                                val userContact = profileDocument.getString("profileContact") ?: ""
+                                                val subTitle = "comment: ${userComment}\ncontact: ${userContact}"
+                                                val profileImage = profileDocument.getString("profileImageUrl") ?: "https://via.placeholder.com/150"
+
+                                                // 가져온 값으로 MusicBuddy 객체 생성하여 리스트에 추가
+                                                musicBuddyList.add(MusicBuddy(userName, subTitle, profileImage))
+                                            }
+
+                                            // RecyclerView 업데이트
+                                            updateRecyclerView()
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Log.e("FindOtherActivity", "Error getting profile documents for user: ${userDocument.id}", e)
+                                        }
                                     break
                                 }
                             }
 
-                            recyclerView = findViewById(R.id.buddyRecyclerView)
-                            recyclerView.layoutManager = LinearLayoutManager(this)
-                            musicBuddyAdapter = MusicBuddyAdapter(this, musicBuddyList)
-                            recyclerView.adapter = musicBuddyAdapter
-                            
-                            Log.d("TAG", "Successfully retrieved user documents")
-                            Toast.makeText(this@FindOtherActivity, "successed", Toast.LENGTH_SHORT).show()
+                            // 모든 다이어리 문서 처리 후 RecyclerView 업데이트
+//                            updateRecyclerView()
+
                         }
                         .addOnFailureListener { e ->
-                            Toast.makeText(this@FindOtherActivity, "failed", Toast.LENGTH_SHORT).show()
-                            Log.e("TAG", "Error getting diaries documents", e)
+                            Log.e("FindOtherActivity", "Error getting diaries documents for user: ${userDocument.id}", e)
                         }
                 }
             }
             .addOnFailureListener { e ->
-                Log.e("TAG", "Error getting users documents", e)
+                Log.e("FindOtherActivity", "Error getting users documents", e)
             }
     }
+
+
+
+    private fun updateRecyclerView() {
+        recyclerView = findViewById(R.id.buddyRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        musicBuddyAdapter = MusicBuddyAdapter(this, musicBuddyList)
+        recyclerView.adapter = musicBuddyAdapter
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
--- app/src/main/java/com/example/androidtermproject/viewmodel/LoginActivity.kt
+++ app/src/main/java/com/example/androidtermproject/viewmodel/LoginActivity.kt
@@ -1,17 +1,21 @@
 package com.example.androidtermproject.viewmodel
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtermproject.databinding.ActivityLoginBinding
 import com.example.androidtermproject.viewmodel.CalendarActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
         // FirebaseAuth 인스턴스 초기화
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
         binding.loginButton.setOnClickListener {
             val id = binding.email.text.toString()
@@ -55,13 +60,50 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // 회원가입 성공
-                    Toast.makeText(this, "Account created and signed in.", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, CalendarActivity::class.java)
-                    startActivity(intent)
+                    val user = auth.currentUser
+                    val uid = user?.uid
+
+                    // 사용자 프로필 문서 생성
+                    if (uid != null) {
+                        val userDocRef = db.collection("users").document(uid)
+                        val userData = hashMapOf(
+                            "userName" to email,
+                        )
+                        userDocRef.set(userData)
+                            .addOnSuccessListener {
+                                // 사용자 프로필 생성 성공
+                                // 프로필 문서에 필요한 데이터 추가
+                                val profileData = hashMapOf(
+                                    "profileName" to email,
+                                    "profileComment" to "",
+                                    "profileContact" to "",
+                                    "profileImageUrl" to ""
+                                )
+                                userDocRef.collection("profile").document("userProfile").set(profileData)
+                                    .addOnSuccessListener {
+                                        // 프로필 데이터 추가 성공
+                                        Toast.makeText(this, "Account created and signed in.", Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this, CalendarActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                    .addOnFailureListener { e ->
+                                        // 프로필 데이터 추가 실패
+                                        Log.e(TAG, "Error creating user profile data", e)
+                                        binding.errorMessage.text = "Sign up failed: ${e.message}"
+                                    }
+                            }
+                            .addOnFailureListener { e ->
+                                // 사용자 프로필 생성 실패
+                                Log.e(TAG, "Error creating user profile document", e)
+                                binding.errorMessage.text = "Sign up failed: ${e.message}"
+                            }
+                    }
+
                 } else {
                     // 회원가입 실패
                     binding.errorMessage.text = "Sign up failed: ${task.exception?.message}"
                 }
             }
     }
+
 }
